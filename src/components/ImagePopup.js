a97adf9c3064623ddc2d256edb9e28e094848794
Maria Serebrennikova <maria.silver2015@yandex.ru>
2022-04-29 15:14:26 +0300

    fix: исправить alt у ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup(props) {
     <section className={`popup popup_type_bigscreen ${props.isOpen ? "popup_active" : ""}`}>
         <div className="popup__container popup__container_type_bigscreen">
           <figure className="image image_no-margin">
-            <img className="popup__image" src={props.card.link} alt="Изображение" />
+            <img className="popup__image" src={props.card.link} alt={`Карточка ${props.card.name}`} />
             <p className="popup__name">{props.card.name}</p>
           </figure>
           <button
